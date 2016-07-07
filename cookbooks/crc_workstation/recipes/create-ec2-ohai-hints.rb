03f6b203c1eae768bc3a84bed1fdf94c7d376264
Ian Henry <ihenry@chef.io>
2016-07-07 17:19:43 -0400

    initial commit

---
+++ cookbooks/crc_workstation/recipes/create-ec2-ohai-hints.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: crc_workstation
+# Recipe:: create-ec2-ohai-hints
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+directory "/etc/chef/ohai/hints" do
+    recursive true
+end
+
+file "/etc/chef/ohai/hints/ec2.json" do
+    content '{}'
+end
